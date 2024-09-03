3a6d199bf36d0ca67885a03b6d3953b39a2e240a
MochaMilkie <mochamilk985@gmail.com>
2024-09-03 01:26:55 -0500

    Began setting up hashmap for config GUI, added another particle trail, added lots of comments.

--- src/main/java/social/gfl/MochaMilkie/gFLUtils/ReloadCommand.java
+++ src/main/java/social/gfl/MochaMilkie/gFLUtils/ReloadCommand.java
@@ -3,20 +3,24 @@ package social.gfl.MochaMilkie.GFLUtils;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.entity.Player;
 
+import java.io.Console;
 import java.io.IOException;
 
 public class ReloadCommand implements CommandExecutor {
     public GFLUtils plugin;
+    private Player player;
 
     public ReloadCommand(GFLUtils plugin){this.plugin = plugin;}
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
         RegEvents events = new RegEvents(plugin);
-        Player player = (Player) commandSender;
-        if(player.hasPermission("GFLUtils.admin")){
+
+        if (commandSender instanceof Player) player = (Player) commandSender;
+        if(commandSender instanceof ConsoleCommandSender || player.hasPermission("GFLUtils.admin")){
             try {
                 plugin.pluginConfig.load(plugin.pluginFile);
             } catch (IOException | InvalidConfigurationException e) {
@@ -24,7 +28,7 @@ public class ReloadCommand implements CommandExecutor {
             }
 
             events.registerMyMethods();
-            player.sendMessage("GFLUtils has reloaded its configuration and registered events.");
+            commandSender.sendMessage("GFLUtils has reloaded its configuration and registered events.");
             return true;
         }
         return false;
